36ce53c01096389d4b1ba7c44f20524e594dd3a2
Andrea Jemmett <andreajemmett@gmail.com>
2015-11-27 12:01:27 +0100

    fixed install script

--- install.sh
+++ install.sh
@@ -18,8 +18,9 @@ fi
 
 # Install NeuroPH
 if [ ! -d 'neuroph' ]; then
-	echo 'Installing MASON...'
+	echo 'Installing NeuroPH...'
 	wget http://netcologne.dl.sourceforge.net/project/neuroph/neuroph-2.92/neuroph-2.92.zip
 	unzip neuroph-2.92.zip
 	rm neuroph-2.92.zip
+	mv neuroph-2.92 neuroph
 fi
