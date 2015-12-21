65d0b905d90a3f58b417103d416410f4ef42b527
Andrea Jemmett <andreajemmett@gmail.com>
2015-12-21 18:24:42 +0100

    plot barcharts

--- report/table.py
+++ report/table.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 # stats_path = "/Users/erotundo/git/UniNotes/ASO/statistics"
@@ -33,7 +35,7 @@ fitness_cols = {
 
 for run in runs:
     num_sub_pop = int(run.split('.')[1].split('v')[0])
-    print run
+    print run + "\n"
     df = pd.DataFrame()
     for i in range(1, 41):
         file_path = stats_path + str("/" + str(run) + "/" + str(i)+'.stat')     
@@ -43,11 +45,36 @@ for run in runs:
         # print mean_dog_fitness
         mean_corralled_ratio = df_run.iloc[:, -5].mean()
         # print mean_corralled_ratio
+        mean_escaped_ratio = df_run.iloc[:, -4].mean()
+        # print mean_escaped_ratio
         
         df.loc[i, "fitness"] = mean_dog_fitness
         df.loc[i, "corralled_ratio"] = mean_corralled_ratio
+        df.loc[i, "escaped_ratio"] = mean_escaped_ratio
 
     # print df
-    print df.loc[:, "fitness"].describe()
-    print df.loc[:, "corralled_ratio"].describe()
-    print ""
+    print str(df.loc[:, "fitness"].describe()) + "\n"
+    print str(df.loc[:, "corralled_ratio"].describe()) + "\n"
+    print str(df.loc[:, "escaped_ratio"].describe()) + "\n\n\n"
+
+
+data1 = dfs[0:3]
+data2 = dfs[0] + dfs[3:]
+data = [data1, data2]
+
+ind = np.arange(2)
+width = 0.35       # the width of the bars
+for d in data:
+    fig, ax = plt.subplots()
+    for df in d:
+        corralled = df.loc[:, "corralled_ratio"]
+        escaped = df.loc[:, "escaped_ratio"]
+        rects1 = ax.bar(ind, [corralled.mean(), escaped.mean()], width, color='r', yerr=[corralled.std(), escaped.std()])
+
+    # add some text for labels, title and axes ticks
+    ax.set_ylabel('Ratio')
+    ax.set_title('')
+    ax.set_xticks(ind + width)
+    ax.set_xticklabels(("Corralled", "Escaped"))
+
+    plt.show()
